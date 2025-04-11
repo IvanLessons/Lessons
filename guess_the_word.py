f165f7976fdad8cfb0ce6366b9b7dd2ec5d0b7e8
IvanLessons <alshlyapik@gmail.com>
2025-04-11 11:06:14 +0300

    Update guess_the_word.py

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,9 +1,10 @@
 import random
+import time
 
 words = {'кошка': {'тема': 'животные', 'подсказка': 'говорит мяу'}, 'собака': {'тема': 'животные', 'подсказка': 'говорит гав'}}
 
 print('Игра "Угадай Слова"!')
-print('В этой тебе будет печататься слово и количество букв в слове, а тебе нужно будет отгадать')
+print('В этой тебе будет печататься количество букв в слове, а тебе нужно будет его отгадать')
 print('Вводить нужно по одной букве за раз')
 
 word = random.choice(list(words.keys()))
@@ -16,10 +17,15 @@ victory = False
 
 print(f'Я загадал слово из {word_len} букв на тему {theme}')
 print('Введи "подсказка" чтобы узнать подсказку')
+print('Это игра на время, у тебя есть 5 минут, чтобы угадать слово')
 
-while tries < 10:
+time_limit = 60 * 5  
+start_time = time.time()  
+
+while tries < 10 and (time.time() - start_time) < time_limit:  
+    print(f'У тебя осталось времени: {(time_limit - (time.time() - start_time)) / 60} минут')  
     print(f'У тебя осталось {10 - tries} попыток')
-    print(result)
+    print(' '.join(result))  
     user_letter = input()
     if user_letter in word:
         print('Такая буква есть!')
@@ -37,9 +43,11 @@ while tries < 10:
         print('Такой буквы нет!')
         tries += 1
 
-
 if victory:
     print('Ты победил!')
+elif (time.time() - start_time) >= time_limit:
+    print('Время вышло! Ты проиграл.')
+    print(f'Было загадано слово "{word}"')
 else:
-    print('Ты проиграл')
+    print('Ты проиграл, попытки закончились.')
     print(f'Было загадано слово "{word}"')
